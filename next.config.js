ddae70fab2cf95ccd68a94bc6a18f34d04b399bb
Edson <shadabkalim375@gmail.com>
2024-01-01 20:38:57 +0530

    final

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  images: {
+    domains: ["uploadthing.com", "utfs.io"],
+  },
+};
+
+module.exports = nextConfig;
